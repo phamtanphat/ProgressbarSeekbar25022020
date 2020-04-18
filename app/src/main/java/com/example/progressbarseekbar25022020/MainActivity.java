e7346caa33e7da9b06f59ec2037dd65152c9f8dd
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-18 20:24:17 +0700

    seekbar

--- app/src/main/java/com/example/progressbarseekbar25022020/MainActivity.java
+++ app/src/main/java/com/example/progressbarseekbar25022020/MainActivity.java
@@ -8,28 +8,28 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    ProgressBar mProgressBar;
+//    ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressBar = findViewById(R.id.progressbar);
-
-        CountDownTimer countDownTimer = new CountDownTimer(1000 , 1000) {
-            @Override
-            public void onTick(long l) {
-                if (mProgressBar.getProgress() >= 100){
-                    mProgressBar.setProgress(0);
-                }
-                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
-            }
-
-            @Override
-            public void onFinish() {
-                this.start();
-            }
-        };
-        countDownTimer.start();
+//        mProgressBar = findViewById(R.id.progressbar);
+//
+//        CountDownTimer countDownTimer = new CountDownTimer(1000 , 1000) {
+//            @Override
+//            public void onTick(long l) {
+//                if (mProgressBar.getProgress() >= 100){
+//                    mProgressBar.setProgress(0);
+//                }
+//                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//                this.start();
+//            }
+//        };
+//        countDownTimer.start();
     }
 }
